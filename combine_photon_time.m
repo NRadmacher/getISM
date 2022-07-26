4f1d5387ea26e9495a1f003df60ab844ab5d55f8
Niels Radmacher <niels.radmacher@stud.uni-goettingen.de>
2022-07-26 17:19:14 +0200

    added new funtion to cambine time trace of photons if sample was scaned multiple times.

---
+++ combine_photon_time.m
@@ -0,0 +1,7 @@
+function y = combine_photon_time(x, tol)
+%COMBINE_PHOTON_TIME removes scan time from photen stream of ISM images
+%with multiple scans
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
